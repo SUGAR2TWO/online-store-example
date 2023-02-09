49b736b027b57ae3b42056edb9eb5384d288d389
SUGAR2TWO <sugar22two@gmail.com>
2023-02-09 15:47:29 +0300

    initial commit

---
+++ src/main/java/com/sugar/onlinestoreexample/OnlineStoreExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sugar.onlinestoreexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreExampleApplication.class, args);
+	}
+
+}
